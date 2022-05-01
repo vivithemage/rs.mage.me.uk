500b5de34d9ae03bca35d170a832e1460847d9d3
Reza Snowdon <rs@mage.me.uk>
2022-05-01 18:46:54 +0100

    minor typos

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -11,35 +11,33 @@ function App() {
           Hello there, welcome to my personal site 😄
         </p>
         <p>I live and work in Darlington, England. In my spare time I work on various projects and experiments.</p>
-        <p>Details can be found on my <a
+        <p>Details can be found on my
+          <a
             className="App-link"
             href="https://blog.vixre.co.uk"
             target="_blank"
             rel="noopener noreferrer"
-          > blog</a>,
+          >blog</a>,
           <a
             className="App-link"
             href="https://gitlab.com/vivithemage"
             target="_blank"
             rel="noopener noreferrer"
-          >
-          gitlab</a> and 
+          >Gitlab</a> and 
           <a
             className="App-link"
             href="https://github.com/vivithemage"
             target="_blank"
             rel="noopener noreferrer"
-          >github</a> accounts. </p>
-        <p>I've also started cycling to work and generally for fun &#128525; &#128690;. </p>
+          >Github</a> accounts. </p>
+        <p>I've also started cycling to work and generally for fun &#128525; &#128690;.</p>
         <p>
           <a
             className="Email-link"
             href="mailto:ribqhev9t@mozmail.com"
             target="_blank"
             rel="noopener noreferrer"
-          >          
-            Get in Touch
-          </a>                    
+          >Get in Touch</a>                    
         </p>
       </header>
     </div>
